0bbae2fa92e3515dcd588febcf12cb9f47d95637
Toby White <toby@datamediate.com>
2020-12-06 12:30:40 +0200

    Rename sanakorakone to sanaporakone

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sanakorakone.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sanaporakone.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- sanaporakone/asgi.py
+++ sanaporakone/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for sanakorakone project.
+ASGI config for sanaporakone project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sanakorakone.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sanaporakone.settings')
 
 application = get_asgi_application()
